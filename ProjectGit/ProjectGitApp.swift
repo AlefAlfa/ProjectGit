d7a0d248ea3d0b1829ae7acb6209c1b68e98fe0c
Lev Lekush <lekushalev@gmail.com>
2024-01-24 06:57:14 +0100

    Initial Commit

---
+++ ProjectGit/ProjectGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectGitApp.swift
+//  ProjectGit
+//
+//  Created by Lev Lekush on 24.01.24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
